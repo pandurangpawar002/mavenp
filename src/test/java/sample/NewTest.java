6edc619e0505d8597bef5a422fec0082d74d198b
pandurangpawar002 <96601058+pandurangpawar002@users.noreply.github.com>
2022-07-11 20:51:17 +0530

    Update NewTest.java

--- src/test/java/sample/NewTest.java
+++ src/test/java/sample/NewTest.java
@@ -4,6 +4,7 @@ import org.testng.annotations.Test;
 import org.testng.annotations.BeforeTest;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 
